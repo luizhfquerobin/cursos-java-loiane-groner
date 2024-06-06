1c585c1f787086ad6c66ffb1df188ac6285b1151
luizhfquerobin <luizhfquerobin@gmail.com>
2024-06-06 10:26:02 -0300

    add aula12, aula13 & aula14

---
+++ eclipse/curso_java_basico/src/com/luiz/cursojava/aula13/OpereadoresAritimeticos.java
@@ -0,0 +1,47 @@
+package com.luiz.cursojava.aula13;
+
+public class OpereadoresAritimeticos {
+
+	public static void main(String[] args) {
+
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		
+		resultado = resultado * 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		String primeiroNome = "Esta é";
+		String segundoNome = " uma string concatenada";
+		String terceiroNome = primeiroNome + segundoNome;
+		System.out.println(terceiroNome);
+		
+		resultado = resultado + 1;
+		System.out.println(resultado);
+		
+		resultado++;
+		System.out.println(resultado);
+		
+		System.out.println(resultado++);
+		System.out.println(++resultado);
+		
+		resultado--;
+		System.out.println(resultado);
+		
+		System.out.println(resultado--);
+		System.out.println(--resultado);
+		
+	}
+
+}
