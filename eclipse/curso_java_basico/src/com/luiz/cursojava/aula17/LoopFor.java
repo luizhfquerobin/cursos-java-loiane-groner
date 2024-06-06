c05cf7dd790f86766b78804b3ae5eca4441b118e
luizhfquerobin <luizhfquerobin@gmail.com>
2024-06-06 14:40:18 -0300

    add aula15, 16, 17, 18, & 19

---
+++ eclipse/curso_java_basico/src/com/luiz/cursojava/aula17/LoopFor.java
@@ -0,0 +1,38 @@
+package com.luiz.cursojava.aula17;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+
+		for (int i = 0	; i < 5; i++) {
+			System.out.println("Valor de i: " + i);
+		}
+		
+		for (int i = 5; i > 0; i--) {
+			System.out.println("Valor de i: " + i);
+		}
+		
+		for (int i = 0, j = 10; i < j; i++, j--) {
+			System.out.println("i = " + i + " j = " + j);
+		}
+		
+		
+		int count = 0;
+		for( ; count < 10 ; ) {
+			System.out.println("count = " + count);
+			count += 2;
+		}
+		
+		for(int cont = 0; cont < 10; cont += 2) {
+			System.out.println("count = " + cont);
+		}
+		
+		int soma = 0;
+		for (int i = 1; i < 5; soma += i++);
+		System.out.println("O valor da soma é: " + soma);
+		
+		for (int i = 0; i < 5; i++)
+			System.out.println("Valor de i: " + i);
+	}
+
+}
