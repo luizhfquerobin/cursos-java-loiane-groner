c678b816ce43cefeb2a6f1031f4a0b94b253e636
luizhfquerobin <luizhfquerobin@gmail.com>
2024-06-15 14:40:15 -0300

    add Aula02 to Aula05 from estruturadados

---
+++ eclipse/estruturadados-algoritimos-java/src/com/luiz/estruturadados/vetor/Vetor.java
@@ -0,0 +1,79 @@
+package com.luiz.estruturadados.vetor;
+
+import java.util.Arrays;
+
+public class Vetor {
+	
+	private String[] elementos;
+	private int tamanho;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+		this.tamanho = 0;
+	}
+	
+	/*public void adiciona(String elemento) {
+		for (int i =0; i < this.elementos.length; i++) {
+			if (this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}*/
+	
+	/*public void adiciona(String elemento) throws Exception {
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos");
+		}
+	
+	}*/
+	
+	public boolean adiciona(String elemento) {
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		} else {
+			return false;
+		}
+	
+	}
+	
+	public String busca(int posicao) {
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Possição inválida");
+		}
+		return this.elementos[posicao];
+	}
+	
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+			
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+		
+		s.append("]");
+		
+		return s.toString();
+		
+	}
+	
+	
+	
+}
---
+++ eclipse/estruturadados-algoritimos-java/src/com/luiz/estruturadados/vetor/teste/Aula05.java
@@ -0,0 +1,17 @@
+package com.luiz.estruturadados.vetor.teste;
+
+import com.luiz.estruturadados.vetor.Vetor;
+
+public class Aula05 {
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(10);
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+		System.out.println(vetor.busca(0));
+		System.out.println(vetor.busca(15));
+	}
+}
