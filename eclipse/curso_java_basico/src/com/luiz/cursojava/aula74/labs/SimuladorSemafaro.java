c2c2b7ff1b9755afae63a2bac5f7b28157dc869f
luizhfquerobin <luizhfquerobin@gmail.com>
2024-07-03 14:56:17 -0300

    add aula71 to aula74

---
+++ eclipse/curso_java_basico/src/com/luiz/cursojava/aula74/labs/SimuladorSemafaro.java
@@ -0,0 +1,18 @@
+package com.luiz.cursojava.aula74.labs;
+
+public class SimuladorSemafaro {
+
+	public static void main(String[] args) {
+		
+		ThreadSemafaro semafaro = new ThreadSemafaro();
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(semafaro.getCor());
+			semafaro.esperaCorMudar();
+		}
+		
+		semafaro.desligarSemafaro();
+
+	}
+
+}
